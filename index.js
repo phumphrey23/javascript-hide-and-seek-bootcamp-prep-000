7838b2898bf8edab6b433e8fbbc36bbd3faff3dc
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-24 15:33:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested .target')
 };
+
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li')
+  for (var i=0; i<ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+};
+
+function deepestChild(){
+  
+};
