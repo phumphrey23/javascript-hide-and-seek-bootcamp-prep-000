34d47257d44de3556f03594d07a2c26604406e95
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-24 15:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getFirstSelector(selector){
-  document.querySelector(selector)
+  return document.querySelector(selector)
 };
 
 function nestedTarget(){
-  document.querySelector('#nested.target')
+  return document.querySelector('#nested.target')
 };
